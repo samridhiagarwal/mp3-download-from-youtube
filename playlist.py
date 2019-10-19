e65589a95db3fbb926500c3559341f6f844d2092
Samridhi Agarwal <samridhia658@IN.nam.ad.pwcinternal.com>
2019-10-19 13:29:57 +0530

    playlist input from user

--- playlist.py
+++ playlist.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
 import youtube_dl
+import sys
 import os
+
+playlist_link = sys.argv[1]
+dl_format = sys.argv[2]
+
 opts = {
-    'format': 'bestaudio/best',
+    'format': dl_format,
     'extractaudio':True,
     'audioformat':'mp3',
     'outtmpl': u'%(title)s.%(ext)s',
@@ -14,6 +19,6 @@ opts = {
 }
 with youtube_dl.YoutubeDL(opts) as ydl:
     #result = ydl.extract_info('https://www.youtube.com/playlist?list=PLZ7UWQadkaDPrv8oFjcc86JoSlaWFAFM7', download = False)
-    ydl.download(['https://www.youtube.com/playlist?list=PLZ7UWQadkaDPrv8oFjcc86JoSlaWFAFM7'])
+    ydl.download([playlist_link])
 
 #os.system("youtube-dl -citk --buffer-size 144 FORMAT <https://www.youtube.com/playlist?list=PLZ7UWQadkaDPrv8oFjcc86JoSlaWFAFM7>")
